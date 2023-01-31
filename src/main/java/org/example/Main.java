d9f6b3153daebaa2dbc600058265e489c13a0d62
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-01-31 15:28:27 +0400

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,6 @@
 package org.example;
 
-import org.example.repository.impl.WriterRepositoryImpl;
-
 public class Main {
     public static void main(String[] args) {
-        WriterRepositoryImpl writerRepository = new WriterRepositoryImpl();
-        writerRepository.deleteById(null);
     }
 }
